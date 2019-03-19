20adc864cdc40e08f428c2c1cef74e8fb9c4ad81
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-03-19 13:42:38 -0400

    Add character + diff camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <gl3w/GL/gl3w.h>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics/Shader.hpp>
+#include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include "noisegen.h"
 #include "renderman.h"
+#include "character.h"
 
-const inline float ZOOM = 20;
-const inline float SPEED = 0.34;
-const inline int LAYERS = 128;
+const inline float ZOOM = 13;
+const inline float SPEED = 0.14;
+const inline int LAYERS = 8;
 const inline int SEED = 19;
 const inline int SIZE = 768;
 constexpr inline float SENS = 0.008;
@@ -35,10 +37,13 @@ int main(int argc, char ** argv) {
 	glClearColor(0, 0, 0, 1);
 
 	// Generate noisemap
-	int noiseTex = noisegen::generateNoiseTexture(SIZE, SIZE, LAYERS, SEED);
-	int colorTex = noisegen::generateNoiseTexture(SIZE / 4, SIZE / 4, LAYERS, SEED + 1);
-	noisegen::DeletingTextureRef _noise_deleter(noiseTex); // use RAII to make sure we erase the thing
-	noisegen::DeletingTextureRef _color_deleter(colorTex); // use RAII to make sure we erase the thing
+	noisegen::NoiseSet noiseMap(SIZE * SIZE * LAYERS);
+
+	// Create textures
+	int noiseTex = noisegen::generateNoiseTexture(SIZE, SIZE, LAYERS, SEED, noiseMap.data);
+	int colorTex = noisegen::generateNoiseTexture(SIZE / 4, SIZE / 4, LAYERS, SEED + 2);
+	noisegen::DeletingTextureRef _noise_deleter(noiseTex); // |
+	noisegen::DeletingTextureRef _color_deleter(colorTex); // |-> use RAII to make sure we erase the thing
 
 	// Load shader
 	renderman::Shader shader;
@@ -54,7 +59,6 @@ int main(int argc, char ** argv) {
 	glm::mat4 projection = glm::perspective(glm::radians(100.0), 1024.0 / 768.0, 0.2, LAYERS * 2.0);
 	glm::mat4 view       = glm::lookAt(glm::vec3(100, 100, 0), glm::vec3(100, 100, -10), glm::vec3(0, 1, 0));
 
-	glm::vec3 position(100, 100, ZOOM);
 	glm::vec4 velocity(0);
 
 	// Load font atlas
@@ -63,11 +67,8 @@ int main(int argc, char ** argv) {
 
 	glEnable(GL_DEPTH_TEST);
 
-	// Setup mouse motion
-	sf::Vector2i center(512, 384);
-	float yaw = 0;
-	float pitch = 0;
-	float roll = 0;
+	// Create character
+	character::Char character(SIZE, SIZE, LAYERS, 2.5, noiseMap.data);
 
 	while (true) {
 		// Handle events
@@ -80,24 +81,11 @@ int main(int argc, char ** argv) {
 					case sf::Event::Resized:
 						glViewport(0, 0, event.size.width, event.size.height);
 						projection = glm::perspective(glm::radians(100.0), (double)event.size.width / event.size.height, 0.2, 300.0);
-						center = sf::Vector2i(event.size.width / 2, event.size.height / 2);
 						break;
 					case sf::Event::KeyPressed:
 						if (event.key.code == sf::Keyboard::Escape) goto die;
-						if (event.key.code == sf::Keyboard::F) {pitch = 1.57; yaw = -1.57; roll = 0;}
+						if (event.key.code == sf::Keyboard::Space) character.jump();
 						break;
-					case sf::Event::MouseMoved:
-						if (event.mouseMove.x == center.x && event.mouseMove.y == center.y) break;
-						{
-							// Offset
-							sf::Vector2i offset = sf::Vector2i(event.mouseMove.x, event.mouseMove.y) - center;
-							sf::Mouse::setPosition(center, window);
-
-							pitch -= (SENS / 2) * offset.y;
-							yaw -= SENS * offset.x;
-
-							pitch = glm::clamp(pitch, 0.001f, 3.14f);
-						}
 					default:
 						break;
 				}
@@ -113,47 +101,9 @@ int main(int argc, char ** argv) {
 		else {
 			velocity.x = 0;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-			velocity.y = -SPEED;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			velocity.y = SPEED;
-		}
-		else {
-			velocity.y = 0;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)) {
-			velocity.z = -SPEED;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-			velocity.z = SPEED;
-		}
-		else {
-			velocity.z = 0;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-			velocity.w = -SPEED / 4;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) {
-			velocity.w = SPEED / 4;
-		}
-		else {
-			velocity.w = 0;
-		}
 
 		// Do rendering
-		glm::vec3 offset_vec = glm::vec3(
-			10 * glm::sin(pitch) * glm::cos(yaw),
-			10 * glm::cos(pitch),
-			-10 * glm::sin(pitch) * glm::sin(yaw)
-		);
-
-		glm::vec3 forward = glm::vec3(-glm::cos(yaw), 0, glm::sin(yaw));
-		glm::vec3 left = glm::cross(forward, glm::vec3(0, 1, 0));
-		position += forward * velocity.y + left * velocity.x + glm::vec3(0, 1, 0) * velocity.z;
-		roll -= (velocity.w / 4);
-
-		view = glm::lookAt(position, position - offset_vec, glm::vec3(glm::sin(roll), glm::cos(roll), 0));
+		view = glm::lookAt(character.get_center() + glm::vec3(0, 0.5, ZOOM), character.get_center(), glm::vec3(0, 1, 0));
 		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -168,6 +118,10 @@ int main(int argc, char ** argv) {
 		glActiveTexture(GL_TEXTURE2); glBindTexture(GL_TEXTURE_3D, colorTex);
 
 		layervao.draw();
+
+		character.draw(matrix);
+		character.set_x_impulse(velocity.x);
+		character.update_physics();
 
 		window.display();
 	}
